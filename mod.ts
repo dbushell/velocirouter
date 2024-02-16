a1dcb764af1d4107f977076a3018b58329492e02
David Bushell <david@dbushell.com>
2024-02-16 11:55:08 +0000

    export types

--- mod.ts
+++ mod.ts
@@ -1,2 +1,15 @@
-export type * from './src/types.ts';
 export * from './src/router.ts';
+export type {
+  Method,
+  AsyncResponse,
+  RequestResponse,
+  HandleResponse,
+  HandleProps,
+  Handle,
+  HandleResolve,
+  Route,
+  Routes,
+  RouterMethod,
+  RouterOptions,
+  Platform
+} from './src/types.ts';
