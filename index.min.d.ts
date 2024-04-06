0683d8a918dd024f6749145aaa815e66488114a0
David Bushell <david@dbushell.com>
2024-04-06 13:28:59 +0100

    improve generic platform types

--- index.min.d.ts
+++ index.min.d.ts
@@ -10,6 +10,9 @@ export type Method =
   | 'PUT'
   | 'TRACE';
 
+/** Platform specific context default type */
+export type Platform = Record<PropertyKey, unknown>;
+
 /** `Response` (asynchronous or resolved) */
 export type AsyncResponse = Response | Promise<Response>;
 
@@ -29,7 +32,7 @@ export type HandleResponse =
   | Promise<undefined | null | Response | RequestResponse>;
 
 /** Properties passed to route handles */
-export interface HandleProps<P> {
+export interface HandleProps<P = Platform> {
   request: Request;
   response?: Response;
   /** Pattern matches from the Request URL */
@@ -41,7 +44,7 @@ export interface HandleProps<P> {
 }
 
 /** Route handle function */
-export interface Handle<P> {
+export interface Handle<P = Platform> {
   (props: HandleProps<P>): HandleResponse;
 }
 
@@ -52,22 +55,22 @@ export interface HandleResolve {
 }
 
 /** Internal route object */
-export type Route<P> = {
+export type Route<P = Platform> = {
   order: number;
   handle: Handle<P>;
   pattern: URLPattern;
 };
 
 /** Internal route object array */
-export type Routes<P> = Array<Route<P>>;
+export type Routes<P = Platform> = Array<Route<P>>;
 
 /** Router class method for attaching HTTP method handles */
-export interface RouterMethod<P> {
+export interface RouterMethod<P = Platform> {
   (pattern: URLPatternInput, ...handle: Array<Handle<P>>): void;
 }
 
 /** Init options for Router class */
-export interface RouterOptions<P> {
+export interface RouterOptions<P = Platform> {
   /** Fallback handle if an error is thrown (500 response is default) */
   onError?: (error: unknown, request: Request, platform: P) => AsyncResponse;
   /** Fallback handle if no matches are found (404 response is default) */
@@ -75,9 +78,6 @@ export interface RouterOptions<P> {
   /** Generate `HEAD` routes for each `GET` route added */
   autoHead?: boolean;
 }
-
-/** Platform specific context default type */
-export type Platform = Record<string | number | symbol, never>;
 
 declare class Router<P = Platform> {
   constructor(options?: RouterOptions<P>);
